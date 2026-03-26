2769742fd501ac2fb2d629517df5c562b811f6ce
Mariaguerrer193 <29marijose10@gmail.com>
2026-03-26 10:15:15 -0500

    diseño de admin para gestion de code de admin

---
+++ Barberia.MVC/Controllers/AdminController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Barberia.Controllers
+{
+    public class AdminController : Controller
+    {
+        
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        
+        [HttpPost]
+        public IActionResult Validar(string codigoAdmin)
+        {
+            
+            const string secreto = "0993587216-JGC";
+
+            if (codigoAdmin == secreto)
+            {
+                
+                return RedirectToAction("Index", "Citas");
+            }
+            else
+            {
+                
+                ViewBag.Error = "CÓDIGO DE SEGURIDAD INCORRECTO";
+                return View("Index");
+            }
+        }
+    }
+}
